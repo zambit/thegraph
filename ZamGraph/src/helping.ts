789b6ebc1b4186d22841a78826a0efcd25657550
Nick Khandarov <khandarovnick@gmail.com>
2022-04-21 15:42:35 +0300

    - wallets from circulation supply

--- ZamGraph/src/helping.ts
+++ ZamGraph/src/helping.ts
@@ -6,6 +6,13 @@ export let ADDRESS_CREATOR = Address.fromHexString('0xa80BB6727BcB8116bBD7355384
 export let ADDRESS_STAKE_REWARDS = Address.fromHexString('0x9633813343e61A70024c266Dd8376E2764641711')
 export let ADDRESS_POOL = Address.fromHexString('0x5dAAFBE1a286c7E03Dcb3F4734415a23A6A2723a')
 export let ADDRESS_BRIDGE = Address.fromHexString('0x0d02e94fB14f19b5E04E4CC91f62c1A80B09a6B6')
+export let ADDRESS_TEAM = Address.fromHexString('0x8B21Eb3a9BB7082a5a159984CE5f7c072Ac7F4Fa')
+export let ADDRESS_ECOSYSTEM = Address.fromHexString('0x2DDb0e4a79D18dD64FFE32631C0aaFa3eef61bF9')
+export let ADDRESS_MARKETING = Address.fromHexString('0x554ff44aa1a9642C607d83762cD8Af0C01A7226f')
+export let ADDRESS_PARTNERSHIPS = Address.fromHexString('0x5D5F5d0b95fCD20d3Fb837d6341Da4B0B02f224b')
+export let ADDRESS_LIQUIDITY = Address.fromHexString('0xCa33617b6e0732f1520b1eA4BF411542D0953660')
+export let ADDRESS_COMMUNITY = Address.fromHexString('0xE18A429F481C92aded138bf9C460b72ea24A058f')
+export let ADDRESS_AFFILIATE = Address.fromHexString('0x294BEEC61b7841d7A3aD86Ee2204b667F5EEF68E')
 export let actual = 'singleton'
 
 
--- ZamGraphBSC/src/financialparameters.ts
+++ ZamGraphBSC/src/financialparameters.ts
@@ -1,6 +1,6 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {Circulation, Blocked, TotalSupply, Holder, VestingCount, StackerCount} from "../generated/schema"
-import { actual, ADDRESS_CREATOR, ADDRESS_POOL, ADDRESS_STAKE_REWARDS } from "./helping"
+import { actual, ADDRESS_CREATOR, ADDRESS_POOL, ADDRESS_STAKE_REWARDS, ADDRESS_TEAM, ADDRESS_ECOSYSTEM, ADDRESS_MARKETING, ADDRESS_PARTNERSHIPS, ADDRESS_LIQUIDITY, ADDRESS_COMMUNITY, ADDRESS_AFFILIATE} from "./helping"
 import { updateBlockedDate, updateCirculationDate } from "./historical"
 
 export function updateParameters(timestamp: BigInt): void {
@@ -30,12 +30,26 @@ export function updateParameters(timestamp: BigInt): void {
     let pool = Holder.load(ADDRESS_POOL.toHex())
     let vesting = VestingCount.load(actual)
     let stacking = StackerCount.load(actual)
+    let team = Holder.load(ADDRESS_TEAM.toHex())
+    let ecosystem = Holder.load(ADDRESS_ECOSYSTEM.toHex())
+    let marketing = Holder.load(ADDRESS_MARKETING.toHex())
+    let partnerships = Holder.load(ADDRESS_PARTNERSHIPS.toHex())
+    let lequidity = Holder.load(ADDRESS_LIQUIDITY.toHex())
+    let community = Holder.load(ADDRESS_COMMUNITY.toHex())
+    let affiliate = Holder.load(ADDRESS_AFFILIATE.toHex())
 
     let balance_creator = BigInt.fromI32(0)
     let balance_stacking_rewards = BigInt.fromI32(0)
     let balance_pool = BigInt.fromI32(0)
     let balance_vesting = BigInt.fromI32(0)
     let balance_stacking = BigInt.fromI32(0)
+    let balance_team = BigInt.fromI32(0)
+    let balance_ecosystem = BigInt.fromI32(0)
+    let balance_marketing = BigInt.fromI32(0)
+    let balance_partnerships = BigInt.fromI32(0)
+    let balance_lequidity = BigInt.fromI32(0)
+    let balance_community = BigInt.fromI32(0)
+    let balance_affiliate = BigInt.fromI32(0)
 
 
     if (creator != null) {
@@ -58,8 +72,37 @@ export function updateParameters(timestamp: BigInt): void {
         balance_stacking = stacking.totalStake
     }
 
+    if (team != null) {
+        balance_team = team.balance
+    }
+
+    if (ecosystem != null) {
+        balance_ecosystem = ecosystem.balance
+    }
+
+    if (marketing != null) {
+        balance_marketing = marketing.balance
+    }
+
+    if (partnerships != null) {
+        balance_partnerships = partnerships.balance
+    }
+
+    if (lequidity != null) {
+        balance_lequidity = lequidity.balance
+    }
+
+    if (community != null) {
+        balance_community = community.balance
+    }
+
+    if (affiliate != null) {
+        balance_affiliate = affiliate.balance
+    }
+
     circulation.circulation = totalSupply.supply.minus(balance_creator).minus(balance_stacking_rewards).minus(balance_pool).
-                                                        minus(balance_vesting).minus(balance_stacking)
+        minus(balance_vesting).minus(balance_stacking).minus(balance_team).minus(balance_ecosystem).minus(balance_marketing).
+        minus(balance_partnerships).minus(balance_lequidity).minus(balance_community).minus(balance_affiliate)
     blocked.blocked = totalSupply.supply.minus(circulation.circulation)
 
     circulation = updateCirculationDate(circulation, timestamp) 
